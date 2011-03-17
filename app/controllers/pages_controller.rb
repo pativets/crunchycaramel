e47f00d6531d4ee783e44bdff588b209dce42966
Pativet Sathiensamrit <pativets@gmail.com>
2011-03-17 14:27:17 -0400

    changed logo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,12 @@
 class PagesController < ApplicationController
   def home
       @title = "Home"
-      @feed_items = Event.all.paginate(:page => params[:page])
+       @feed_items = Event.all.paginate(:page => params[:page])
       # @feed_item1, @feed_item2, etc
+  #    @feed_item1 = Event.find(1)
+  #    @feed_item2 = Event.find(2)
+  #    @feed_item3 = Event.find(3)
+  #    @feed_item4 = Event.find(4)
   end
 
   def contact
