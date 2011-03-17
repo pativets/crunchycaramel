0639a3e9a7ec456ddb610b4c708e53a239406371
Pativet Sathiensamrit <pativets@gmail.com>
2011-03-17 17:55:56 -0400

    fixed error

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,7 @@
 class PagesController < ApplicationController
   def home
       @title = "Home"
-      @feed_item1 = Event.find(4)
-      @feed_item2 = Event.find(5)
-      @feed_item3 = Event.find(6)
-      @feed_item4 = Event.find(4)
+
   end
 
   def contact
