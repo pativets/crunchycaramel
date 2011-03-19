8c5c021b18a608e19c4818be61e061186dbb4669
Joonha Shin <shin.joonha@gmail.com>
2011-03-19 13:59:28 -0400

    Cleaned up code for Event model.

--- app/models/event.rb
+++ app/models/event.rb
@@ -21,24 +21,26 @@
 
 class Event < ActiveRecord::Base
 
-	attr_accessible :description, :event_title, :event_type, :occurence, :startdate, :starttime, :location, :enddate, :endtime
+	attr_accessible :description, :event_title, :event_type, :occurence, 
+	                :startdate, :starttime, :location, :enddate, :endtime
 	
 	belongs_to:user
 	
-	validates :description, :presence => true, :length => {:maximum => 140}
-	validates :user_id, :presence => true
-	validates :event_title, :presence => true, :length => {:maximum => 50}
-	validates :event_type, :presence => true
-	validates :occurence, :presence => true
-	validates :event_type, :presence => true
-	validates :startdate, :presence => true
-	validates :starttime, :presence => true
-	validates :enddate, :presence => true
-	validates :endtime, :presence => true
-	validates :location, :presence => true
+	validates :description, :presence => true,
+	                        :length => {:maximum => 140}
+	validates :user_id,     :presence => true
+	validates :event_title, :presence => true,
+	                        :length => {:maximum => 50}
+	validates :event_type,  :presence => true
+	validates :occurence,   :presence => true
+	validates :event_type,  :presence => true
+	validates :startdate,   :presence => true
+	validates :starttime,   :presence => true
+	validates :enddate,     :presence => true
+	validates :endtime,     :presence => true
+	validates :location,    :presence => true
 
-
-OCCURENCE = ['One-Time', 'Daily', 'Weekly', 'Montly', 'Annual']
-EVENT_TYPE = ['Free', 'Cheap']
+  OCCURENCE = ['One-Time', 'Daily', 'Weekly', 'Monthly', 'Annual']
+  EVENT_TYPE = ['Free', 'Cheap']
 
 end
