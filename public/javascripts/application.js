918992de3052122e3e25432601a2235060955325
Joonha Shin <shin.joonha@gmail.com>
2011-03-19 13:47:15 -0400

    Added javascript to event form. Price field toggle activated.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,17 @@
-
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-
+function price_toggle(){
+  var selectmenu=document.getElementById("event_event_type")
+  selectmenu.onchange=function(){ //run some code when "onchange" event fires
+    var chosenoption=this.options[this.selectedIndex] //this refers to "selectmenu"
+    if (chosenoption.value=="Cheap"){
+      document.getElementById('price_field').setAttribute("class", "data_row");
+    }
+    else if (chosenoption.value=="Free"){
+      document.getElementById('price_field').setAttribute("class", "hidden_elem");
+    }
+    else {
+      // do nothing for now...
+    }
+  }
+}
