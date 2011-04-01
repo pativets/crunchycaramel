87019c8ea32f46cc4e1998c8b3a0f80473449693
Joonha Shin <shin.joonha@gmail.com>
2011-03-31 22:26:17 -0400

    Fixed more conflicts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,13 +2,6 @@ class PagesController < ApplicationController
   def home
     @title = "Home"
     @feed_items = Event.all.paginate #must include .paginate
-<<<<<<< HEAD
-    # @feed_item1 = Event.find(1)
-    # @feed_item2 = Event.find(2)
-    # @feed_item3 = Event.find(3)
-    # @feed_item4 = Event.find(4)
-=======
->>>>>>> facebox
   end
 
   def contact
